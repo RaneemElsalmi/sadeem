ab7b76cef3709dfadaf98f1aafcaf0859ba45eff
RaneemElsalmi <152659566+RaneemElsalmi@users.noreply.github.com>
2024-09-10 20:28:56 +0300

    Add files via upload

--- vendors.go
+++ vendors.go
@@ -1,34 +1,50 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
-	
+
 	"os"
-	"strings"
 	"test/controllers/utils"
 	"test/controllers/utils/models"
+	"strings"
 )
 
 var (
-	Domainv = os.Getenv("DOMAIN")
+	Domainv        = os.Getenv("DOMAIN")
 	vendor_columns = []string{
 		"id",
 		"name",
-		"img",
 		"description",
 		"created_at",
 		"updated_at",
+		fmt.Sprintf("CASE WHEN NULLIF(img, '') IS NOT NULL THEN FORMAT('%s/%%s', img) ELSE NULL END AS img", Domainv),
 	}
 )
 
 func IndexvendorHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, vendors!")) 
+	var vendors []models.Vendor
+
+	query, args, err := QB.Select(strings.Join(vendor_columns, ", ")).
+		From("vendors").
+		ToSql()
+
+	if err != nil {
+		utils.HandleError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	if err := db.Select(&vendors, query, args...); err != nil {
+		utils.HandleError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	utils.SendJSONResponse(w, http.StatusOK, vendors)
 }
-func ShowvendorHandler(w http.ResponseWriter, r *http.Request) {
+
+func ShowvenderHandler(w http.ResponseWriter, r *http.Request) {
 	var vendor []models.Vendor
 	id := r.PathValue("id")
 	query, args, err := QB.Select(strings.Join(vendor_columns, ", ")).
-	From("vendor").
+		From("vendor").
 		Where("id = ?", id).
 		ToSql()
 	if err != nil {
